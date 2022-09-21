3734fc36a03eddc61c5de0c216d18a4c26740b03
lrbn86 <lrbn86@gmail.com>
2022-09-20 22:54:46 -0400

    refactor DayOfWeek.java: fall thru switch statement to get day type

--- DayOfWeek.java
+++ DayOfWeek.java
@@ -3,35 +3,25 @@ public class DayOfWeek {
     public static void main(String[] args) {
 
         int day = 4;
-        String dayName = "";
+        String dayType = "";
 
         switch (day) {
             case 1:
-                dayName = "Monday";
-                break;
             case 2:
-                dayName = "Tuesday";
-                break;
             case 3:
-                dayName = "Wednesday";
-                break;
             case 4:
-                dayName = "Thursday";
-                break;
             case 5:
-                dayName = "Friday";
+                dayType = "Weekday";
                 break;
             case 6:
-                dayName = "Saturday";
-                break;
             case 7:
-                dayName = "Sunday";
+                dayType = "Weekend";
                 break;
             default:
-                dayName = "Invalid day";
+                dayType = "Invalid day";
         }
 
-        System.out.println("The day is " + dayName);
+        System.out.println(dayType);
 
     }
     
