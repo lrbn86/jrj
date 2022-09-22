a62183a5e90e75bd065baa2ae93edc072162d12b
lrbn86 <lrbn86@gmail.com>
2022-09-22 11:48:02 -0400

    ClassRoster: create ClassRosterController.java

---
+++ ClassRoster/src/controller/ClassRosterController.java
@@ -0,0 +1,50 @@
+package controller;
+
+import ui.ClassRosterView;
+import ui.UserIO;
+import ui.UserIOConsoleImpl;
+
+public class ClassRosterController {
+
+    private ClassRosterView view = new ClassRosterView();
+    private UserIO io = new UserIOConsoleImpl();
+
+    public void run() {
+
+        boolean keepGoing = true;
+        int menuSelection = 0;
+        while (keepGoing) {
+
+            menuSelection = getMenuSelection();
+
+            switch(menuSelection) {
+                case 1:
+                    io.print("LIST STUDENTS");
+                    break;
+                case 2:
+                    io.print("CREATE STUDENT");
+                    break;
+                case 3:
+                    io.print("VIEW STUDENT");
+                    break;
+                case 4:
+                    io.print("REMOVE STUDENT");
+                    break;
+                case 5:
+                    keepGoing = false;
+                    break;
+                default:
+                    io.print("UNKNOWN COMMAND");
+            }
+        }
+
+        io.print("GOOD BYE");
+        io.close();
+
+    }
+
+    private int getMenuSelection() {
+        return view.printMenuAndGetSelection();
+    }
+    
+}
