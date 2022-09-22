50752c4223960b6f5fefd38f62b84a5c943875c9
lrbn86 <lrbn86@gmail.com>
2022-09-22 11:47:35 -0400

    ClassRoster: create App.java

---
+++ ClassRoster/src/App.java
@@ -0,0 +1,12 @@
+import controller.ClassRosterController;
+
+public class App {
+
+    public static void main(String[] args) throws Exception {
+
+        ClassRosterController controller = new ClassRosterController();
+        controller.run();
+        
+    }
+
+}
