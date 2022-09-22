9ed5a145ff07b77a32661d1d9d5e20ecd8c9994b
lrbn86 <lrbn86@gmail.com>
2022-09-22 12:04:33 -0400

    ClassRoster: update ClassRosterDaoFileImpl.java

--- ClassRoster/src/dao/ClassRosterDaoFileImpl.java
+++ ClassRoster/src/dao/ClassRosterDaoFileImpl.java
@@ -1,5 +1,33 @@
 package dao;
 
-public class ClassRosterDaoFileImpl {
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import dto.Student;
+
+public class ClassRosterDaoFileImpl implements ClassRosterDao {
+
+    final Map<String, Student> students = new HashMap<>();
+
+    @Override
+    public Student addStudent(String studentId, Student student) {
+        return students.put(studentId, student);
+    }
+
+    @Override
+    public List<Student> getAllStudents() {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    @Override
+    public Student getStudent(String studentId) {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    @Override
+    public Student removeStudent(String studentId) {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
     
 }
